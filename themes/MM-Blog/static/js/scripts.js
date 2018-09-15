19a6d0b0a69da1af3a1c4bdda3a1c550e9e332e0
AmosGarner <aagarner.7@gmail.com>
2018-09-15 01:13:39 -0400

    Init nav, footer, and scripts files.

---
+++ themes/MM-Blog/static/js/scripts.js
@@ -0,0 +1,7 @@
+const displayElement = (element) => {
+    element.children[1].classList.remove("hiden");
+};
+
+const hideElement = (element) => {
+    element.children[1].classList.add("hiden");
+};
